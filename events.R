3cdf848a9808a080379bf3432b3150a2b8a5a1c2
Felix Su <felixs8696@berkeley.edu>
2016-05-05 18:34:30 -0700

    added packages and formatted correct dates

--- events.R
+++ events.R
@@ -1,19 +1,25 @@
 library(DataComputing)
 library(rvest)
 library(lubridate)
+library(XML)
 applestocks <- read.csv("http://real-chart.finance.yahoo.com/table.csv?s=AAPL&d=3&e=29&f=2016&g=d&a=11&b=12&c=1980&ignore=.csv") %>%
   mutate(appleclose=Close, Date=lubridate::ymd(Date), applevolume=Volume)
 timeline <- htmlParse("http://www.timetoast.com/timelines/apple-product-release-dates")
 root <- xmlRoot(timeline)
 dates <- root %>% xpathSApply("//time",xmlValue)
 date <- c()
+#for(i in 1:length(dates)){
+#  date <- c(date, as.numeric(lubridate::ymd(as.Date(dates[i], "%B %dst, %Y"))))
+#}
 for(i in 1:length(dates)){
-  date <- c(date, lubridate::ymd(as.Date(dates[i], "%B %dst, %Y")))
+  date <- c(date, toString(as.Date(dates[i], "%B %dst, %Y")))
 }
 desc <- root %>% xpathSApply("//td/b",xmlValue)
-timeframe <- data.frame(date, desc)
+timeframe <- data.frame(date, desc) %>%
+  mutate(date=lubridate::ymd(date))
 ggplot(applestocks, aes(x=Date,y=appleclose)) + 
   geom_line() +
   geom_vline(data = timeframe, aes(xintercept = as.numeric(date), col=desc)) +
-  theme(legend.text=element_text(size = 10))+ labs(title="Apple Stock Prices and Product Release Dates",x="Date", y="Stock Close", col='Event')+
-  scale_color_viridis(discrete=TRUE) + theme_light()
+  theme(legend.text=element_text(size = 10),
+        plot.title = element_text(size = 15)) +
+  labs(title="Apple Stock Prices and Product Release Dates",x="Date", y="Stock Close", col='Event')
